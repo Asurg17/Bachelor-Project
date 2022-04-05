f9014c5752beed94448ecc33315dbcd5f437c146
Asurg17 <48888626+Asurg17@users.noreply.github.com>
2022-04-05 22:10:52 +0400

    dev: initial commit

---
+++ SociaLight/SociaLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SociaLight
+//
+//  Created by Sandro Surguladze on 05.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
